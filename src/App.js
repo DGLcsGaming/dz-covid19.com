26af2c556c85eeee4e92961ab87fc831e437b93b
Fayçal Ghoul <dglcsgaming@gmail.com>
2020-03-27 22:38:50 +0100

    Fixed URL

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import { globalContext } from "./contexts/globalContext";
 import useWindowDimensions from "./hooks/useWindowDimensions";
 import openSocket from "socket.io-client";
 
-const socket = openSocket("http://localhost:4000");
+const socket = openSocket("http://212.24.98.17:4000");
 
 function App() {
   const [globalState, setGlobalState] = useState({
