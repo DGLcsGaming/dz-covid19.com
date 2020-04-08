3e37eb80e614d653a01bbeb412a978bdd25a94a9
Fayçal Ghoul <dglcsgaming@gmail.com>
2020-04-08 18:41:34 +0100

    r

--- src/App.js
+++ src/App.js
@@ -178,9 +178,9 @@ function App() {
     }
   }, [cookies, loading, isConsentGranted, userSubscription, pushServerSubscriptionId]);
 
-  useEffect(() => {
+  /*  useEffect(() => {
     alert("الموقع يتعرض لضغط كبير الآن, سيتم إصلاح المشكل بعد قليل, نعتذر عن ذلك");
-  }, []);
+  }, []); */
   var content;
 
   if (!isServerDown) {
