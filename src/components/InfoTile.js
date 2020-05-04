c03d23e438e298cd28cb80a0521afd70587286aa
Fayçal Ghoul <faycalghoul@gmail.com>
2020-05-04 16:35:09 +0100

    y

--- src/components/InfoTile.js
+++ src/components/InfoTile.js
@@ -109,14 +109,14 @@ function InfoTile() {
           </div>
         </div>
       </div>
-      {/* <AdSense.Google
+      <AdSense.Google
         client="ca-pub-3044347590723152"
         slot="7748570575"
         style={{ display: "block" }}
         format="auto"
         responsive="true"
         layoutKey="-gw-1+2a-9x+5c"
-      /> */}
+      />
     </div>
   );
 }
