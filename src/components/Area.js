854b642297155292b76b0d4e98da5a4eeeaa0a7c
Fayçal Ghoul <faycalghoul@gmail.com>
2020-05-07 01:25:09 +0100

    y

--- src/components/Area.js
+++ src/components/Area.js
@@ -1,17 +1,39 @@
 import React, { useContext } from "react";
 import { globalContext } from "../contexts/globalContext";
+import { useTranslation } from "react-i18next";
 
 function Area(props) {
   const [globalState, setGlobalState] = useContext(globalContext);
+  const { t } = useTranslation();
   const handleClick = () => {
     setGlobalState({ ...globalState, selectedWilayaId: props.data.code });
   };
   return (
     <div id={props.data.name} className="area" onClick={handleClick}>
       <div className="areaName">{props.data.name}</div>
+      <div style={{ margin: "5px 0px", borderBottom: "1px solid rgb(221, 221, 221)", width: "80%", lineHeight: "1px" }}>&nbsp;</div>
       <div className="areaTotal">
-        <strong>{props.data.confirmed}</strong>
-        <div className="delta">+{props.data.new_cases}</div>
+        <div className="areaStats areaConfirmed">
+          <strong>{t("General.Confirmed")}</strong>
+          <div>
+            {props.data.confirmed}
+            <div className="delta">+{props.data.new_cases}</div>
+          </div>
+        </div>
+        <div className="areaStats areaRecovered">
+          <strong>{t("General.Recovered")}</strong>
+          <div>
+            {props.data.recovered}
+            <div className="delta">+{props.data.new_cases}</div>
+          </div>
+        </div>
+        <div className="areaStats areaDeaths">
+          <strong>{t("General.Deaths")}</strong>
+          <div>
+            {props.data.deaths}
+            <div className="delta">+{props.data.new_cases}</div>
+          </div>
+        </div>
       </div>
     </div>
   );
